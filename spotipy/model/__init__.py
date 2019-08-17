9c9da40c2f073ae25a7275b4d56feaa89ad69687
Felix Hildén <felix.hilden@visma.com>
2019-08-17 23:26:28 +0300

    PEP 8 import line lengths

--- spotipy/model/__init__.py
+++ spotipy/model/__init__.py
@@ -1,15 +1,41 @@
-from spotipy.model.album import AlbumType, AlbumGroup, ReleaseDatePrecision, Album, SimpleAlbum, SavedAlbum
+from spotipy.model.album import (
+    AlbumType,
+    AlbumGroup,
+    ReleaseDatePrecision,
+    Album,
+    SimpleAlbum,
+    SavedAlbum
+)
 from spotipy.model.album.full import FullAlbum
 from spotipy.model.artist import Artist, SimpleArtist, FullArtist
 from spotipy.model.audio_features import Key, Mode, AudioFeatures
 from spotipy.model.category import Category
 from spotipy.model.context import Context
 from spotipy.model.disallows import Disallows
-from spotipy.model.error import PlayerErrorReason, Error, PlayerError, AuthenticationError
+from spotipy.model.error import (
+    PlayerErrorReason,
+    Error,
+    PlayerError,
+    AuthenticationError
+)
 from spotipy.model.member import Copyright, Followers, Image, Restrictions
 from spotipy.model.paging import Paging, OffsetPaging, Cursor, CursorPaging
 from spotipy.model.play_history import PlayHistory
-from spotipy.model.playlist import PlaylistTrack, PlaylistTrackPaging, Playlist, SimplePlaylist, FullPlaylist
+from spotipy.model.playlist import (
+    PlaylistTrack,
+    PlaylistTrackPaging,
+    Playlist,
+    SimplePlaylist,
+    FullPlaylist
+)
 from spotipy.model.recommendations import RecommendationSeed, Recommendations
-from spotipy.model.track import TrackLink, Track, SimpleTrack, FullTrack, SimpleTrackPaging, Tracks, SavedTrack
+from spotipy.model.track import (
+    TrackLink,
+    Track,
+    SimpleTrack,
+    FullTrack,
+    SimpleTrackPaging,
+    Tracks,
+    SavedTrack
+)
 from spotipy.model.user import User, PrivateUser, PublicUser
