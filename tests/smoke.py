2c649c6ec9fbab254f510976b04a963ce875c684
felix <felix.hilden@gmail.com>
2019-08-18 19:07:42 +0300

    Import smoke tests

---
+++ tests/smoke.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+class Import(unittest.TestCase):
+    """
+    Import parts of the system to check for
+    e.g. syntax errors and cyclic imports.
+    """
+    @staticmethod
+    def test_import_spotipy():
+        import spotipy
+
+    @staticmethod
+    def test_import_client():
+        from spotipy import client
+
+    @staticmethod
+    def test_import_model():
+        from spotipy import model
+
+    @staticmethod
+    def test_import_auth():
+        from spotipy import auth
+
+    @staticmethod
+    def test_import_convert():
+        from spotipy import convert
+
+    @staticmethod
+    def test_import_scope():
+        from spotipy import convert
+
+
+if __name__ == '__main__':
+    unittest.main()
