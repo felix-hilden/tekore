c90bc984029910359b196261f20f3d314e94369c
felix <felix.hilden@gmail.com>
2020-11-26 11:12:41 +0200

    Combine seed requirement information

--- tekore/_client/api/browse/__init__.py
+++ tekore/_client/api/browse/__init__.py
@@ -197,11 +197,9 @@ class SpotifyBrowse(SpotifyBase):
         """
         Get a list of recommended tracks for seeds.
 
-        Up to 5 seed values may be provided as artists, genres and tracks.
-
         .. warning::
-            At least one of artist_ids, genres or track_ids must be
-            defined, otherwise Spotify returns an "invalid request" error.
+            The total number of seeds provided in ``artist_ids``, ``genres``
+            and ``track_ids`` must be at least 1 and at most 5.
 
         Parameters
         ----------
