5c0caa73310b1e2fcb87dbeae44a1d01a2feee1b
Felix Hildén <felix.hilden@gmail.com>
2025-12-14 16:57:00 +0200

    Happy early new year!

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -10,7 +10,7 @@ from tekore import __version__
 
 project = "tekore"
 author = "Felix Hildén"
-copyright = "2019-2025, Felix Hildén"
+copyright = "2019-2026, Felix Hildén"
 version = __version__
 release = version
 
